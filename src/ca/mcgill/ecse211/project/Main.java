e9f282f2daec1e9d21cb730747888742a1e952dc
Aakarsh Shekhar <aakarsh.shekhar@mail.mcgill.ca>
2019-10-20 18:03:52 -0400

    adding some new classes
    Localization
    Main implementation from lab 4

--- src/ca/mcgill/ecse211/project/Main.java
+++ src/ca/mcgill/ecse211/project/Main.java
@@ -1,9 +1,91 @@
 package ca.mcgill.ecse211.project;
 
+import static ca.mcgill.ecse211.project.Resources.*;
+import ca.mcgill.ecse211.project.UltrasonicPoller;
+import ca.mcgill.ecse211.project.Display;
+import lejos.hardware.Button;
+import lejos.robotics.SampleProvider;
+
 /**
- * The main class.
+ * 
+ * @author Aakarsh
+ * @author Steven
+ * @author Hassan
  */
+
 public class Main {
+  //initilalizes Ultrasonic Sensor
+  static SampleProvider distance = US_SENSOR.getMode("Distance");
+  static float[] sampleUS = new float[distance.sampleSize()];
+  final static UltrasonicPoller UP = new UltrasonicPoller(distance, sampleUS);
+
+  public static void main(String[] args) {
+    LightLocalizer lightLocalize = new LightLocalizer();
+    UltrasonicLocalizer localize;
+    int option;
+    option = chooseEdge();
+    new Thread(new Display()).start();
+    new Thread(odometer).start();
+    //choose between Falling Edge and Rising Edge
+    if (option == Button.ID_LEFT) {
+      localize = new UltrasonicLocalizer(0, US_SENSOR);
+      localize.localize();
+    } else if (option == Button.ID_RIGHT) {
+      localize = new UltrasonicLocalizer(1, US_SENSOR);
+      localize.localize();
+    } else {
+      showErrorAndExit("Error - invalid button!");
+    }
+    //Start light localization when ultrasonic localizationi is over
+    lightLocalize.localize();
+  }
+
+  /**
+   * Sleeps current thread for the specified duration.
+   * 
+   * @param duration sleep duration in milliseconds
+   */
+  public static void sleepFor(long duration) {
+    try {
+      Thread.sleep(duration);
+    } catch (InterruptedException e) {
+      // There is nothing to be done here
+    }
+  }
+
+  /**
+   * Shows error and exits program.
+   * 
+   * @param String
+   */
+  public static void showErrorAndExit(String errorMessage) {
+    LCD.clear();
+    System.err.println(errorMessage);
+
+    // Sleep for 2 seconds so user can read error message
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+    }
+
+    System.exit(-1);
+  }
+
+  /**
+   * Asks the user to choose in-between falling edge and rising edge
+   * 
+   * @return the user choice
+   */
+  private static int chooseEdge() {
+    int buttonChoice;
+    Display.showText("< Left | Right >", "falling| rising ", " edge  |  edge  ", "       |        ",
+        "       |        ");
+
+    do {
+      buttonChoice = Button.waitForAnyPress();
+    } while (buttonChoice != Button.ID_LEFT && buttonChoice != Button.ID_RIGHT);
+    return buttonChoice;
+  }
 }
 
 
---
+++ src/ca/mcgill/ecse211/project/UltrasonicPoller.java
@@ -0,0 +1,42 @@
+package ca.mcgill.ecse211.project;
+
+import lejos.robotics.SampleProvider;
+
+/**
+ * Simple UltrasonicPoller that samples the US sensor 
+ * and method returns true
+ * when obstacle is detected
+ * 
+ * @author Aakarsh 
+ * @author Hassan
+ * 
+ */
+public class UltrasonicPoller {
+  private float[] sampleUS;
+  SampleProvider distance;
+  int wallDistance;
+  
+  public UltrasonicPoller(SampleProvider distance, float[] sampleUS) {
+    this.distance = distance;
+    this.sampleUS = sampleUS;
+  }
+  
+  /**
+   * 
+   * @return float distance in cm
+   */
+  public float getDistance(){
+    distance.fetchSample(sampleUS, 0);
+    wallDistance = (int) (sampleUS[0] * 100.0); // extract from buffer, convert to cm, cast to int
+    try {
+        Thread.sleep(50);
+    } catch (InterruptedException e) {}
+    if(sampleUS[0]>255){
+        wallDistance = 255;
+    }
+    return wallDistance;
+}
+
+ 
+
+}
