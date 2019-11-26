00e1aedf4fe9ad6336938b5b492603ab6bd3e13c
Steven <luu.stevenluu@gmail.com>
2019-11-26 13:37:26 -0500

    javadocced Main

--- src/ca/mcgill/ecse211/project/Main.java
+++ src/ca/mcgill/ecse211/project/Main.java
@@ -38,7 +38,7 @@ public class Main {
     }
     System.out.println("Map:\n" + wifiParameters);
     LCD.clear();
-    //new Thread(new Display()).start(); // TODO Comment out when presenting
+    // new Thread(new Display()).start(); // TODO Comment out when presenting
     Region tunnel = tng;
     Region startIsland = green;
     Point bin = greenBin;
@@ -60,68 +60,68 @@ public class Main {
     }
 
 
-    
+
     // localize
     localize(startingCorner);
 
     Sound.beep();
     Sound.beep();
     Sound.beep();
-    
 
-//
-//    // compute tunnel coordinates
-//    computeTunnelCoordinates(tunnel);
-//    // Navigation.travelTo(2, 2);
-//
-//    // navigate to tunnel entrance
-//    // turn to face tunnel
-//    // navigate through tunnel
-//    Navigation.travelTo(tunnelStartX, tunnelStartY);
-//
-//    Navigation.turnTo(Math.toRadians(tunnelTheta));
-//    localizeForward(tunnelTheta, true, null);
-//    if (!checkOutOfBounds(tunnelTheta + 90, startIsland)) {
-//      Navigation.turnTo(Math.toRadians(tunnelTheta + 90));
-//      localizeForward(tunnelTheta + 90, true, null);
-//      Navigation.goMid(true);
-//    } else {
-//      Navigation.turnTo(Math.toRadians(tunnelTheta - 90));
-//      localizeForward(tunnelTheta - 90, true, null);
-//      Navigation.goMid(true);
-//    }
-//    // Navigation.turnTo(Math.toRadians(tunnelTheta - 90));
-//    // localizeForward(tunnelTheta - 90, true, null);
-//    // Navigation.goMid();
-//    Navigation.turnTo(Math.toRadians(tunnelTheta));
-//    odometer.setXYT(tunnelStartX * TILE_SIZE, tunnelStartY * TILE_SIZE, tunnelTheta);
-//    Navigation.travelTo(tunnelEndX, tunnelEndY);
-//
-//    /*
-//     * 4.Each machine localizes to the grid. When completed, the machine must stop and issue a sequence of 3 beeps.
-//     *
-//     */
-//
-//    Sound.beep();
-//    Sound.beep();
-//    Sound.beep();
-//
-//    localizeForward(tunnelTheta, false, island);
-//
-//
-//    // TODO: ensure robot stays within island
-//    // calculate and move to launch point
-//    //Navigation.travelTo(bin.x, bin.y, RADIUS);
-//
-//    /*
-//     * 5.Each machine navigates to their corresponding tunnel, transits, and then proceeds to their launch point. Upon
-//     * arriving, each machine will again stop and issue a sequence of 3 beeps.
-//     */
-//
-//    // Navigation.launchPosition(bin.x, bin.y, RADIUS);
-//    // Navigation.travelTo(bin.x, bin.y, RADIUS);
-//    // Navigation.travelTo(bin.x, bin.y);
-//    // Navigation.turnTo(Math.toRadians(tnr.ur.x));
+
+    //
+    // // compute tunnel coordinates
+    // computeTunnelCoordinates(tunnel);
+    // // Navigation.travelTo(2, 2);
+    //
+    // // navigate to tunnel entrance
+    // // turn to face tunnel
+    // // navigate through tunnel
+    // Navigation.travelTo(tunnelStartX, tunnelStartY);
+    //
+    // Navigation.turnTo(Math.toRadians(tunnelTheta));
+    // localizeForward(tunnelTheta, true, null);
+    // if (!checkOutOfBounds(tunnelTheta + 90, startIsland)) {
+    // Navigation.turnTo(Math.toRadians(tunnelTheta + 90));
+    // localizeForward(tunnelTheta + 90, true, null);
+    // Navigation.goMid(true);
+    // } else {
+    // Navigation.turnTo(Math.toRadians(tunnelTheta - 90));
+    // localizeForward(tunnelTheta - 90, true, null);
+    // Navigation.goMid(true);
+    // }
+    // // Navigation.turnTo(Math.toRadians(tunnelTheta - 90));
+    // // localizeForward(tunnelTheta - 90, true, null);
+    // // Navigation.goMid();
+    // Navigation.turnTo(Math.toRadians(tunnelTheta));
+    // odometer.setXYT(tunnelStartX * TILE_SIZE, tunnelStartY * TILE_SIZE, tunnelTheta);
+    // Navigation.travelTo(tunnelEndX, tunnelEndY);
+    //
+    // /*
+    // * 4.Each machine localizes to the grid. When completed, the machine must stop and issue a sequence of 3 beeps.
+    // *
+    // */
+    //
+    // Sound.beep();
+    // Sound.beep();
+    // Sound.beep();
+    //
+    // localizeForward(tunnelTheta, false, island);
+    //
+    //
+    // // TODO: ensure robot stays within island
+    // // calculate and move to launch point
+    // //Navigation.travelTo(bin.x, bin.y, RADIUS);
+    //
+    // /*
+    // * 5.Each machine navigates to their corresponding tunnel, transits, and then proceeds to their launch point. Upon
+    // * arriving, each machine will again stop and issue a sequence of 3 beeps.
+    // */
+    //
+    // // Navigation.launchPosition(bin.x, bin.y, RADIUS);
+    // // Navigation.travelTo(bin.x, bin.y, RADIUS);
+    // // Navigation.travelTo(bin.x, bin.y);
+    // // Navigation.turnTo(Math.toRadians(tnr.ur.x));
     navigateToLaunchPosition(bin);
 
     Sound.beep();
@@ -136,7 +136,7 @@ public class Main {
     localizeForward(tunnelTheta, true, island);
     odometer.setXYT(tunnelStartX * TILE_SIZE, tunnelStartY * TILE_SIZE, tunnelTheta);
     Navigation.travelTo(tunnelEndX, tunnelEndY);
-    Navigation.travelTo(startIsland.ll.x, startIsland.ll.y);
+    navigateToStart(startingCorner);
 
     /*
      * 8.Upon returning to the starting corner, each robot halts and issues a sequence of 5 beeps.
@@ -153,9 +153,12 @@ public class Main {
     System.exit(0);
   }
 
+
   /**
    * Method to run the robot localization First, runs ultrasonic localization, then runs the light localization and
    * corrects theta
+   * 
+   * @param startingCorner, 0,1,2 or 3 depending of parameters sent by wifi
    */
   public static void localize(int startingCorner) {
     LightLocalizer lightLocalize = new LightLocalizer();
@@ -189,21 +192,24 @@ public class Main {
   }
 
   /**
-   * Computes launch point for the robot and navigates to it
+   * Calculaes closest launch position to device and navigates to it
+   * 
+   * @param bin, Point of bin
    */
   public static void navigateToLaunchPosition(Point bin) {
     double[] pos = odometer.getXYT();
     double[] lp = Navigation.launchPosition(bin.x, bin.y, RADIUS, pos[0], pos[1]);
-    if(!validPoint(lp[0], lp[1], island)) {
+    if (!validPoint(lp[0], lp[1], island)) {
       lp = navigateToAlternateLaunchPosition(lp, bin);
     }
 
-    Navigation.travelTo(lp[0]/TILE_SIZE, lp[1]/TILE_SIZE, bin);
-    Navigation.turnTo(findAngle(odometer.getXYT()[0], odometer.getXYT()[1], bin.x, bin.y));    
+    Navigation.travelTo(lp[0] / TILE_SIZE, lp[1] / TILE_SIZE, bin);
+    Navigation.turnTo(findAngle(odometer.getXYT()[0], odometer.getXYT()[1], bin.x, bin.y));
   }
-  
+
   /**
    * Computes the angle between any 2 given points
+   * 
    * @param curr_x current x position of the robot
    * @param curr_y current y position of the robot
    * @param x final x position
@@ -211,17 +217,17 @@ public class Main {
    * @return angle between the 2 points
    */
   public static double findAngle(double curr_x, double curr_y, double x, double y) {
-    return Math.atan2(x*TILE_SIZE-curr_x, y*TILE_SIZE-curr_y);
+    return Math.atan2(x * TILE_SIZE - curr_x, y * TILE_SIZE - curr_y);
   }
 
   /**
-   * In case the original launch position computed is not within the island, we compute alternative
-   * launch positions. We do this by simply moving clockwise on the circle of radius RADIUS around the bin
+   * In case the original launch position computed is not within the island, we compute alternative launch positions. We
+   * do this by simply moving clockwise on the circle of radius RADIUS around the bin
    *
    * @param lp current launch position
    */
   public static double[] navigateToAlternateLaunchPosition(double[] lp, Point bin) {
-    while(!validPoint(lp[0], lp[1], island.ll.x, island.ll.y, island.ur.x, island.ur.y)) {
+    while (!validPoint(lp[0], lp[1], island.ll.x, island.ll.y, island.ur.x, island.ur.y)) {
       lp = Navigation.alternateLaunchPosition(bin, RADIUS, lp);
     }
     return lp;
@@ -239,37 +245,53 @@ public class Main {
    * @return true if point lies within the defined region, else false
    */
   public static boolean validPoint(double x, double y, double ll_x, double ll_y, double ur_x, double ur_y) {
-    return (x>ll_x*TILE_SIZE && x<ur_x*TILE_SIZE && y>ll_y*TILE_SIZE && y<ur_y*TILE_SIZE);
+    return (x > ll_x * TILE_SIZE && x < ur_x * TILE_SIZE && y > ll_y * TILE_SIZE && y < ur_y * TILE_SIZE);
   }
+
+  /**
+   * Computes whether given point (x,y) lies within the defined region
+   * 
+   * @param x defines the given point
+   * @param y defines the given point
+   * @param region
+   * @return true if point lies within the defined region, else false
+   */
   public static boolean validPoint(double x, double y, Region region) {
-    return (x>region.ll.x*TILE_SIZE && x<region.ur.x*TILE_SIZE && y>region.ll.y*TILE_SIZE && y<region.ur.y*TILE_SIZE);
+    return (x > region.ll.x * TILE_SIZE && x < region.ur.x * TILE_SIZE && y > region.ll.y * TILE_SIZE
+        && y < region.ur.y * TILE_SIZE);
   }
 
-
+  /**
+   * Return true if place to localize is within region
+   * 
+   * @param angle, angle in degrees at which device desires to localize
+   * @param region
+   * @return true if place to localize is within region, else false
+   */
   public static boolean checkOutOfBounds(double angle, Region region) {
     double x = odometer.getXYT()[0];
     double y = odometer.getXYT()[1];
     if (angle > 340 && angle < 20) {
-      if (y +TILE_SIZE/2 > region.ur.y * TILE_SIZE)
+      if (y + TILE_SIZE / 2 > region.ur.y * TILE_SIZE)
         return true;
-    }
-    else if (angle < 110 && angle > 70) {
-      if (x + TILE_SIZE/2 > region.ur.x * TILE_SIZE)
+    } else if (angle < 110 && angle > 70) {
+      if (x + TILE_SIZE / 2 > region.ur.x * TILE_SIZE)
         return true;
-    }
-    else if (angle < 200 && angle > 160) {
-      if (y - TILE_SIZE/2 > region.ll.y * TILE_SIZE)
+    } else if (angle < 200 && angle > 160) {
+      if (y - TILE_SIZE / 2 < region.ll.y * TILE_SIZE)
         return true;
-    }
-    else if (angle < 290 && angle > 250) {
-      if (x - TILE_SIZE/2 > region.ll.x * TILE_SIZE)
+    } else if (angle < 290 && angle > 250) {
+      if (x - TILE_SIZE / 2 < region.ll.x * TILE_SIZE)
         return true;
     }
     return false;
   }
 
+
   /**
    * Method to compute the coordinates of the tunnel that the robot moves to
+   * 
+   * @param tunnel
    */
   public static void computeTunnelCoordinates(Region tunnel) {
     double x0;
@@ -331,6 +353,32 @@ public class Main {
    */
   public static boolean horizontalTunnel(Region tunnel) {
     return (Math.abs(tunnel.ur.y - tunnel.ll.y) == 1);
+  }
+  
+  /**
+   * Method for device to travel back to its corner
+   * 
+   * @param corner, int given by wifi parameter
+   */
+  public static void navigateToStart(int corner) {
+    double x, y;
+    if (corner == 0) {
+      x = 0;
+      y = 0;
+    }
+    else if (corner == 1) {
+      x = 15;
+      y = 0;
+    }
+    else if (corner == 2) {
+      x = 15;
+      y = 9;
+    }
+    else {
+      x = 0;
+      y = 9;
+    }
+    Navigation.travelTo(x, y);
   }
 
   /**
